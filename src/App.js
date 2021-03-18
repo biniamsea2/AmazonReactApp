2f172b23387328fe7bd897207c54c9e612860923
Biniam <Biniamt23@yahoo.com>
2021-03-17 21:29:37 -0700

    fixed import error, and routes created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    // put everything in the router
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/checkout">
+            <h1>Checkout route</h1>
+          </Route>
+          <Router path="/login">
+            <h1>Login page</h1>
+          </Router>
+          {/* last route is always the default route */}
+          <Route path="/">
+            <h1>Home page</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
