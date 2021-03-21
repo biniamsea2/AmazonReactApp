f5e6faf9946a86dc9f9ab45f33c5fcffbc692780
Biniam <Biniamt23@yahoo.com>
2021-03-21 00:06:18 -0700

    working on css for login page, form almost finished

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './Header';
 import Home from './Home';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from './Checkout';
+import Login from "./Login";
 
 function App() {
   return (
@@ -16,7 +17,7 @@ function App() {
             <Checkout/>
           </Route>
           <Route path="/login">
-            <h1>Login page</h1>
+            <Login/>
           </Route>
           {/* last route is always the default route */}
           <Route path="/">
--- src/Login.js
+++ src/Login.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import './Login.css';
+import React from "react";
+import "./Login.css";
+import { Link, useHistory } from "react-router-dom";
 
 function Login() {
-    return (
-        <div>
-            <h1>Login Page</h1>
-        </div>
-    );
+  return (
+    <div className="login">
+      <Link to="/">
+        <img
+          className="login_logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+          alt=""
+        />
+      </Link>
+      <div className="login_container">
+        <h1>Sign In</h1>
+        <form>
+          <h5>E-mail</h5>
+          <input type="text" />
+          <h5>Password</h5>
+          <input type="text" />
+          <button>Continue</button>
+        <p className="policy">By continuing, you agree to Amazon's Conditions of Use and Privacy Notice.</p>
+        </form>
+      </div>
+
+      <p>New to Amazon?</p>
+      <button>Create your Amazon account</button>
+
+    </div>
+  );
 }
 
-export default Login
+export default Login;
