3700b77250295a20abe4978c3cf9e463a2a615ad
Biniam <Biniamt23@yahoo.com>
2021-03-21 02:44:49 -0700

    setting up auth, almost complete, just need to work on redirecting user to homapge after registering the user

--- src/Login.js
+++ src/Login.js
@@ -4,28 +4,35 @@ import { Link, useHistory } from "react-router-dom";
 import{auth} from "./Firebase";
 
 function Login() {
+    const history = useHistory();
     // checking email 
     const[email, setEmail] = useState('');
     const[password, setPassword] = useState('');
 
-
-
-    const login = event =>{
+    const login = (event) =>{
         //stops refresh
         event.preventDefault()
         //login logic
         auth.signInWithEmailAndPassword(email, password)
-    }
+        .then((auth) => {
+            //logged in, redirect to homepage
+            history.push('/');
+        })
+        .catch(e => alert(e.message));
+    };
 
-    const register = event =>{
+    const register = (event) =>{
         //stops refresh
         event.preventDefault()
         //register logic
-    }
-
-
-
-
+        auth.createUserWithEmailAndPassword(email, password)
+            .then(auth =>{
+                //created a user and logged in, redirect to homepage
+                history.push('/');
+            })
+            .catch(e => alert(e.message));
+        };
+    
   return (
     <div className="login">
       <Link to="/">
@@ -39,9 +46,9 @@ function Login() {
         <h1>Sign In</h1>
         <form>
           <h5>E-mail</h5>
-          <input value = {email} type="email" />
+          <input value = {email} onChange={event =>setEmail(event.target.value)} type="email" />
           <h5>Password</h5>
-          <input value = {password} type="password" />
+          <input value = {password} onChange={event =>setPassword(event.target.value)} type="password" />
           <button onClick={login} type ="submit"className="login_signInButton">Continue</button>
         <p>By continuing, you agree to Amazon's <a href="https://www.amazon.com/gp/help/customer/display.html/ref=ap_signin_notification_condition_of_use?ie=UTF8&nodeId=508088">Conditions of Use </a> and <a href="https://www.amazon.com/gp/help/customer/display.html/ref=ap_signin_notification_privacy_notice?ie=UTF8&nodeId=468496">Privacy Notice</a>.</p>
         </form>
@@ -51,6 +58,6 @@ function Login() {
 
     </div>
   );
-}
+  }
 
 export default Login;
