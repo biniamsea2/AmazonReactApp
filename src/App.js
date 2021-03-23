597164fba10208a9e809c6a4695c777879cd8ca5
Biniam <Biniamt23@yahoo.com>
2021-03-22 20:56:45 -0700

    listener setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect} from "react";
 import "./App.css";
 import Header from './Header';
 import Home from './Home';
@@ -6,9 +6,39 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from './Checkout';
 import Login from "./Login";
 import {useStateValue} from "./StateProvider";
+import {auth} from "./Firebase";
 
-function App() {
-  const[{basket}, dispatch] = useStateValue();
+function App() { 
+  //used for the listener
+  const[{ basket }, dispatch] = useStateValue();
+  
+  // code which runs on a given condition......useEffect
+  useEffect(() => {
+    //create a listener that pays attention when the user logs in and out
+    //everytime the authentication changes, get the authUser,
+    const unsubscribe = auth.onAuthStateChanged((authUser) =>{
+      if(authUser) {
+        //user is logged in..push them into the data layer
+        dispatch({
+          type:"SET_USER",
+          user: authUser
+        })
+      }else{
+        //the user is logged out. set the user to null
+        dispatch({
+          type: "SET_USER",
+          user: null
+        })
+      }
+    });
+    return () => {
+      // clean up operation
+      unsubscribe();
+    }
+    
+    //will run once when the app component loads and everytime when changes are made to whatever we put in the []
+  }, []);
+
 
 
   
