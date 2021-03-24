bfbd8a8f18b257839cde204206ed667a7a3e21c9
Biniam <Biniamt23@yahoo.com>
2021-03-23 21:01:27 -0700

    images for home page added in new folder

--- src/CarouselContainer.js
+++ src/CarouselContainer.js
@@ -1,5 +1,11 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
+import image1 from './HomePageImages/1.jpg';
+import image2 from './HomePageImages/2.jpg';
+import image3 from './HomePageImages/3.jpg';
+import image4 from './HomePageImages/4.jpg';
+import image5 from './HomePageImages/5.jpg';
+import image6 from './HomePageImages/6.jpg';
 
 const CarouselContainer = () => {
   return (
@@ -7,7 +13,7 @@ const CarouselContainer = () => {
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonMusic/2021/Marketing/EvergreenQ1/Gateway/US-EN_AMF_EvergreenQ1_DMUX-3799_JZ_OnS_GW_Hero_D_3000x1200_2X_CV4._CB411870756_.jpg"
+          src={image1}
           alt="First slide"
         />
       </Carousel.Item>
